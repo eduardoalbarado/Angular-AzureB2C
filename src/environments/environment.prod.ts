50bd39b0ce494c3f5aebe4c66b4e16f2e591073b
Eduardo Albarado <eduardoalbarado@gmail.com>
2022-05-31 18:38:21 -0300

    Add-TOKENS-to-be-replace
    Add ${TOKENS} TAGs to be repalced with environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  B2C_clientId: 'Enter_Your_ClientId',
-  B2C_signUpSignIn: 'https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_sign-up',
-  B2C_editProfile: "https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_edit_profile",
-  B2C_authorityDomain: '/Enter_Your_Domain.b2clogin.com',
-  API_url: 'https://localhost:44351/api/todolist',
-  API_scopes: 'https:///Enter_Your_Domain.onmicrosoft.com/Enter_Your_App/ScopeName',
+  B2C_clientId: "${B2C_clientId}",//'Enter_Your_ClientId',
+  B2C_signUpSignIn: "${B2C_signUpSignIn}",//'https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_sign-up',
+  B2C_editProfile: "${B2C_editProfile}",//"https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_edit_profile",
+  B2C_authorityDomain: "${B2C_authorityDomain}",//'/Enter_Your_Domain.b2clogin.com',
+  API_url: "${API_url}",//'https://localhost:44351/api/todolist',
+  API_scopes: "${API_scopes}",//'https:///Enter_Your_Domain.onmicrosoft.com/Enter_Your_App/ScopeName',
 };
