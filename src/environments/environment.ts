182874bef4a5d84ca85ba22f0bbc009296a5151a
Eduardo Albarado <eduardoalbarado@gmail.com>
2022-05-31 19:06:01 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  B2C_clientId: "${B2C_clientId}",//'Enter_Your_ClientId',
-  B2C_signUpSignIn: "${B2C_signUpSignIn}",//'https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_sign-up',
-  B2C_editProfile: "${B2C_editProfile}",//"https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_edit_profile",
-  B2C_authorityDomain: "${B2C_authorityDomain}",//'/Enter_Your_Domain.b2clogin.com',
-  API_url: "${API_url}",//'https://localhost:44351/api/todolist',
-  API_scopes: "${API_scopes}",//'https:///Enter_Your_Domain.onmicrosoft.com/Enter_Your_App/ScopeName',
+  B2C_clientId: "${B2C_CLIENTID}",//'Enter_Your_ClientId',
+  B2C_signUpSignIn: "${B2C_SIGNUPSIGNIN}",//'https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_sign-up',
+  B2C_editProfile: "${B2C_EDITPROFILE}",//"https://Enter_Your_Domain.b2clogin.com/Enter_Your_Domain.onmicrosoft.com/Enter_Your_Policy_edit_profile",
+  B2C_authorityDomain: "${B2C_AUTHORITYDOMAIN}",//'/Enter_Your_Domain.b2clogin.com',
+  API_url: "${API_URL}",//'https://localhost:44351/api/todolist',
+  API_scopes: "${API_SCOPES}",//'https:///Enter_Your_Domain.onmicrosoft.com/Enter_Your_App/ScopeName',
 };
 
 /*
